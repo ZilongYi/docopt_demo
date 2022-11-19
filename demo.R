5e59e2a40097f2f6b76f4a3e27d2d75f09686ee0
Zilong Yi <73105919+ZilongYi@users.noreply.github.com>
2022-11-19 12:16:14 -0800

    Including print statement in the main function.

--- demo.R
+++ demo.R
@@ -1,31 +1,22 @@
 # author: Tiffany Timbers
 # date: 2020-01-15
 
-"""This script prints out docopt args.
-Usage: demo.py <arg1> <arg_new>--arg2=<arg2> [--arg3=<arg3>] 
+"This script prints out docopt args.
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
 
 Options:
-<arg1>            Takes any value (this is a required positional argument)
-<arg_new>         Takes any value (this is a required positional argument)
+<arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-""" 
+[--arg4=<arg4>]   Takes any value (this is an optional option)
+" -> doc
 
-import os
-import pandas as pd
-from docopt import docopt
+library(docopt)
 
-opt = docopt(__doc__)
+main <- fuction{
+    opt <- docopt(doc)
+    print(opt)
+    print(typeof(opt))
+}
 
-def main(url, out_file):
-    data = pd.read_csv(url, header=None)
-    try:
-        data.to_csv(out_file, index=False)
-    except:
-        os.makedirs(os.path.dirname(out_file))
-        data.to_csv(out_file, index=False)
-
-
-if __name__ == "__main__":
-    main(opt["--url"], opt["--out_file"])
-
+main()
