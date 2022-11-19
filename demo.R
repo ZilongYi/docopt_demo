421af2461a0559315bbba962fd787263909fb9a8
Zilong Yi <73105919+ZilongYi@users.noreply.github.com>
2022-11-19 11:59:31 -0800

    Update demo.R
    Adding main function to demo.R

--- demo.R
+++ demo.R
@@ -11,7 +11,21 @@ Options:
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 """ 
 
+import os
+import pandas as pd
 from docopt import docopt
+
 opt = docopt(__doc__)
-print(opt)
-print(type(opt))
+
+def main(url, out_file):
+    data = pd.read_csv(url, header=None)
+    try:
+        data.to_csv(out_file, index=False)
+    except:
+        os.makedirs(os.path.dirname(out_file))
+        data.to_csv(out_file, index=False)
+
+
+if __name__ == "__main__":
+    main(opt["--url"], opt["--out_file"])
+
